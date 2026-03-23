a2ba341fe3eb1c3d870b033b08f725af5d1665b6
Mr.PessiBot <pessibot@users.noreply.github.com>
2026-03-23 14:45:54 +0000

    Change background color

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x1a1a2e);
+scene.background = new THREE.Color(0x24103a);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
